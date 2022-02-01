f310fdf35929f82d40dedf6c2fcba7fc64d1cb3f
liveOLKA <live.olga.bugai@gmail.com>
2022-02-01 19:07:26 +0300

    HomeWork

--- javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -1,4 +1,44 @@
 package school.lesson1;
 
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 23, b = -12;
+        int res = a + b;
+        if (res >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 154;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value < 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 214;
+        int b = -74;
+        String res = a >= b ? ("a >= b") : ("a < b");
+        System.out.println(res);
+    }
 }
